474b028d3e6c74ec799baf7e2f7f4cf40a7b165f
DravenL9 <teddy06@list.ru>
2026-01-15 22:44:38 +0300

    Update CalibrateServos.py
    cal

--- dingo_ws/src/dingo_hardware_interfacing/dingo_servo_interfacing/src/dingo_servo_interfacing/CalibrateServos.py
+++ dingo_ws/src/dingo_hardware_interfacing/dingo_servo_interfacing/src/dingo_servo_interfacing/CalibrateServos.py
@@ -92,7 +92,7 @@ else:
                 #  2  [front_right_lower, front_left_lower, back_right_lower, back_left_lower]] """
 
 offsets = np.array(
-                    [[75, 110, 110, 70],
+                    [[75, 108, 111, 68],
                     [0, 0, 0, 0],
                     [0, 0, 0, 0]])
 
